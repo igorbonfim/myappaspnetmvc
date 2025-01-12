a9a9a773c4e64f6943244e0267433dfb912e4243
Igor Bonfim <igorbonfim88@gmail.com>
2025-01-12 01:21:00 -0300

    One to one relationships

--- MyApp/Data/MyAppContext.cs
+++ MyApp/Data/MyAppContext.cs
@@ -7,6 +7,18 @@ namespace MyApp.Data
     {
         public MyAppContext(DbContextOptions<MyAppContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>().HasData(
+                    new Item { Id = 999, Name = "Microphone", Price = 40, SerialNumberId = 10 }
+                );
+            modelBuilder.Entity<SerialNumber>().HasData(
+                    new SerialNumber { Id = 10, Name = "MIC123456", ItemId = 999  }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Item> Items { get; set; }
+        public DbSet<SerialNumber> SerialNumbers { get; set; }
     }
 }
